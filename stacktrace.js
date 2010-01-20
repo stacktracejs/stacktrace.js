088e12dbd50ffe6847cc85a05b2ee294d344d7ca
Eric Wendelin <emwendelin@gmail.com>
2010-01-20 11:09:08 -0700

    adding initial javascript

---
+++ stacktrace.js
@@ -0,0 +1,202 @@
+// Domain Public by Eric Wendelin http://eriwen.com/ (2008)
+//                  Luke Smith http://lucassmith.name/ (2008)
+//                  Loic Dachary <loic@dachary.org> (2008)
+//                  Johan Euphrosine <proppy@aminche.com> (2008)
+//
+// Information and discussions
+// http://jspoker.pokersource.info/skin/test-printstacktrace.html
+// http://eriwen.com/javascript/js-stack-trace/
+// http://pastie.org/253058
+// http://browsershots.org/http://jspoker.pokersource.info/skin/test-printstacktrace.html
+//
+
+//
+// guessFunctionNameFromLines comes from firebug
+//
+// Software License Agreement (BSD License)
+//
+// Copyright (c) 2007, Parakey Inc.
+// All rights reserved.
+//
+// Redistribution and use of this software in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above
+//   copyright notice, this list of conditions and the
+//   following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above
+//   copyright notice, this list of conditions and the
+//   following disclaimer in the documentation and/or other
+//   materials provided with the distribution.
+//
+// * Neither the name of Parakey Inc. nor the names of its
+//   contributors may be used to endorse or promote products
+//   derived from this software without specific prior
+//   written permission of Parakey Inc.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+// FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+// IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+// OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+function printStackTrace(options) {
+    if (options && options.guess) {
+    var p = new printStackTrace.implementation();
+    var result = p.run();
+    return p.guessFunctions(result);
+    }
+    return (new printStackTrace.implementation()).run();
+}
+
+printStackTrace.implementation = function() {};
+
+printStackTrace.implementation.prototype = {
+    run: function() {
+        mode = this.mode();
+        if(mode != 'other') {
+            try {(0)();} catch (e) {
+                return this[mode](e);
+            }
+        } else {
+            return this.other(arguments.callee);
+        }
+    },
+
+    mode: function() {
+        var mode;
+        try {(0)();} catch (e) {
+            mode = e.stack ? 'firefox' : window.opera ? 'opera' : 'other';
+        }
+        return mode;
+    },
+    
+    firefox: function(e) {
+        return e.stack.replace(/^.*?\n/,'').
+        replace(/(?:\n@:0)?\s+$/m,'').
+        replace(/^\(/gm,'{anonymous}(').
+        split("\n");
+    },
+
+    opera: function(e) {
+        var lines = e.message.split("\n"),
+        ANON = '{anonymous}',
+        lineRE = /Line\s+(\d+).*?script\s+(http\S+)(?:.*?in\s+function\s+(\S+))?/i,
+        i,j,len;
+
+        for (i=4,j=0,len=lines.length; i<len; i+=2) {
+            if (lineRE.test(lines[i])) {
+                lines[j++] = (RegExp.$3 ?
+                              RegExp.$3 + '()@' + RegExp.$2 + RegExp.$1 :
+                              ANON + '()@' + RegExp.$2 + ':' + RegExp.$1) +
+                    ' -- ' + lines[i+1].replace(/^\s+/,'');
+            }
+        }
+
+        lines.splice(j,lines.length-j);
+        return lines;
+    },
+
+    other: function(curr) {
+        var ANON = "{anonymous}",
+        fnRE  = /function\s*([\w\-$]+)?\s*\(/i,
+        stack = [],j=0,
+        fn,args;
+
+        while (curr) {
+            fn = fnRE.test(curr.toString()) ? RegExp.$1 || ANON : ANON;
+            args = Array.prototype.slice.call(curr['arguments']);
+            stack[j++] = fn + '(' + printStackTrace.implementation.prototype.stringifyArguments(args) + ')';
+            curr = curr.caller;
+        }
+
+        return stack;
+    },
+
+    stringifyArguments: function(args) {
+        for (var i = 0; i < args.length; ++i) {
+            var argument = args[i];
+            if (typeof argument  == 'object') {
+            args[i] = '#object';
+            } else if (typeof argument == 'function') {
+            args[i] = '#function';
+            } else if (typeof argument == 'string') {
+            args[i] = '"'+argument+'"';
+            }
+        }
+        return args.join(',');
+    },
+    
+    sourceCache : {},
+
+    ajax: function(url) {
+    return jQuery.ajax({
+            url: url,
+            async: false
+        }).responseText;    
+    },
+
+    getSource: function(url) {
+        var self = this;
+        if (!(url in self.sourceCache)) {
+            self.sourceCache[url] = self.ajax(url).split("\n");
+        }
+        return self.sourceCache[url];
+    },
+
+    guessFunctions: function(stack) {
+        for (var i = 0; i < stack.length; ++i) {
+            var reStack = /{anonymous}\(.*\)@(.*):(\d+)/;
+            var frame = stack[i];
+            var m = reStack.exec(frame);
+            if (m) {
+                var file = m[1];
+                var lineno = m[2];
+                if (file && lineno) {
+                    var functionName = this.guessFunctionName(file, lineno);
+                    stack[i] = frame.replace('{anonymous}', functionName);
+                }
+            }
+        }
+        return stack;
+    },
+
+    guessFunctionName: function(url, lineNo) {      
+        var source;
+        try {
+            source = this.getSource(url);
+        } catch (e) {
+            return 'getSource failed with url: ' + url + ', exception: ' + e.toString();
+        }
+        return this.guessFunctionNameFromLines(lineNo, source);
+    },
+
+    guessFunctionNameFromLines: function(lineNo, source) {
+        var reFunctionArgNames = /function ([^(]*)\(([^)]*)\)/;
+        var reGuessFunction = /['"]?([0-9A-Za-z_]+)['"]?\s*[:=]\s*(function|eval|new Function)/;
+        // Walk backwards from the first line in the function until we find the line which
+        // matches the pattern above, which is the function definition
+        var line = "";
+        var maxLines = 10;
+        for (var i = 0; i < maxLines; ++i) {
+            line = source[lineNo-i] + line;
+            if (line !== undefined) {
+                var m = reGuessFunction.exec(line);
+                if (m) {
+                    return m[1];
+                } else {
+                    m = reFunctionArgNames.exec(line);
+                } 
+                if (m && m[1]) {
+                    return m[1];
+                }
+            }
+        }
+        return "(?)";
+    }
+};
---
+++ test-stacktrace.js
@@ -0,0 +1,315 @@
+//
+//     Copyright (C) 2008 Loic Dachary <loic@dachary.org>
+//     Copyright (C) 2008 Johan Euphrosine <proppy@aminche.com>
+//     Copyright (C) 2010 Eric Wendelin <emwendelin@gmail.com>
+//
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+module("printstacktrace");
+
+test("printStackTrace", function() {
+    expect(1);
+    var r = printStackTrace();
+    equals(typeof r, 'object', 'printStackTrace returns an array');
+});
+
+test("printStackTrace options", function() {
+    expect(1);
+    var guessFunctions = printStackTrace.implementation.prototype.guessFunctions;
+    printStackTrace.implementation.prototype.guessFunctions = function() {
+        printStackTrace.implementation.prototype.guessFunctions = guessFunctions;
+        ok(true, 'guessFunctions called');
+    };
+    var r = printStackTrace({guess: true});
+});
+
+test("mode", function() {
+    expect(1);
+    equals("firefox other opera".indexOf(printStackTrace.implementation.prototype.mode()) >= 0,true);
+});
+
+test("run mode", function() {
+    expect(1);
+    var p = new printStackTrace.implementation();
+    p.other = p.firefox = p.opera = function() { equals(1,1,'called'); };
+    p.run();
+});
+
+test("run firefox", function() {
+    expect(1);
+    var p = new printStackTrace.implementation();
+    p.mode = function() { return 'firefox'; };
+    p.other = p.opera = function() { equals(1,0,'must not be called'); };
+    p.firefox = function() { equals(1,1,'called'); };
+    p.run();
+});
+
+test("run opera", function() {
+    expect(1);
+    var p = new printStackTrace.implementation();
+    p.mode = function() { return 'opera'; };
+    p.other = p.firefox = function() { equals(1,0,'must not be called'); };
+    p.opera = function() { equals(1,1,'called'); };
+    p.run();
+});
+
+test("run other", function() {
+    expect(1);
+    var p = new printStackTrace.implementation();
+    p.mode = function() { return 'other'; };
+    p.opera = p.firefox = function() { equals(1,0,'must not be called'); };
+    p.other = function() { equals(1,1,'called'); };
+    p.run();
+});
+
+test("firefox", function() {
+    var mode = printStackTrace.implementation.prototype.mode();
+    var e = [];
+    e.push({ stack: 'discarded()...\nf1(1,"abc")@file.js:40\n()@file.js:41\n@:0  \nf44()@file.js:494'});
+    if(mode == 'firefox') {
+        function discarded() {
+            try {(0)();} catch (exception) {
+                e.push(exception);
+            }
+        }
+        function f1(arg1, arg2) {
+            discarded();
+        }
+        var f2 = function() {
+            f1(1, "abc");
+        };
+        f2();
+    }
+    expect(4 * e.length);
+    for(var i = 0; i < e.length; i++) {
+        var message = printStackTrace.implementation.prototype.firefox(e[i]);
+        var message_string = message.join("\n");
+        //            equals(message_string, '', 'debug');
+        equals(message_string.indexOf('discarded'), -1, 'discarded');
+        equals(message[0].indexOf('f1(1,"abc")') >= 0, true, 'f1');
+        equals(message[1].indexOf('{anonymous}()@') >= 0, true, 'f2 anonymous');
+        equals(message[2].indexOf('@:0'), -1, '@:0 discarded');
+    }
+});
+
+test("opera", function() {
+    var mode = printStackTrace.implementation.prototype.mode();
+    var e = [];
+    e.push({ message: 'ignored\nignored\nignored\nignored\nLine 40 of linked script http://site.com: in function f1\n      discarded()\nLine 44 of linked script http://site.com\n \tf1(1, "abc")\nignored\nignored'});
+    if(mode == 'opera') {
+        function discarded() {
+            try {(0)();} catch (exception) {
+                e.push(exception);
+            }
+        }
+        function f1(arg1, arg2) {
+            discarded();
+        }
+        var f2 = function() {
+            f1(1, "abc");
+        };
+        f2();
+    }
+    expect(5 * e.length);
+    for(var i = 0; i < e.length; i++) {
+        var message = printStackTrace.implementation.prototype.opera(e[i]);
+        var message_string = message.join("\n");
+        equals(message_string.indexOf('ignored'), -1, 'ignored');
+        equals(message[0].indexOf('f1()') >= 0, true, 'f1 function name');
+        equals(message[0].indexOf('discarded()') >= 0, true, 'f1 statement');
+        equals(message[1].indexOf('{anonymous}()@') >= 0, true, 'f2 is anonymous');
+        equals(message[1].indexOf('f1(1, "abc")') >= 0, true, 'f2 statement');
+    }
+});
+
+test("other", function() {
+    var mode = printStackTrace.implementation.prototype.mode();
+    var frame = function(args, fun, caller) {
+        this['arguments'] = args;
+        this.caller = caller;
+        this.fun = fun;
+    };
+    frame.prototype.toString = function() { return 'function '+this.fun +'() {}'; };
+    function f10() {}
+    var frame_f2 = new frame([], '', undefined);
+    var frame_f1 = new frame([1, 'abc', f10, {1: {2: {3: 4} } }], 'FUNCTION f1  (a,b,c)', frame_f2);
+    expect(mode == 'other' ? 4 : 2);
+    var message = printStackTrace.implementation.prototype.other(frame_f1);
+    var message_string = message.join("\n");
+    equals(message[0].indexOf('f1(1,"abc",#function,#object)') >= 0, true, 'f1');
+    equals(message[1].indexOf('{anonymous}()') >= 0, true, 'f2 anonymous');
+    if(mode == 'other') {
+        function f1(arg1, arg2) {
+            var message = printStackTrace.implementation.prototype.other(arguments.callee);
+            var message_string = message.join("\n");
+            //equals(message_string, '', 'debug');
+            equals(message[0].indexOf('f1(1,"abc",#function,#object)') >= 0, true, 'f1');
+            equals(message[1].indexOf('{anonymous}()') >= 0, true, 'f2 anonymous');
+        }
+        var f2 = function() {
+            f1(1, 'abc', f10, {1: {2: {3: 4} } });
+        };
+        f2();
+    }
+});
+
+test("stringify", function() {
+    expect(3);
+    equals(printStackTrace.implementation.prototype.stringifyArguments(["a", 1, {}, function() {}]), '"a",1,#object,#function');
+    equals(printStackTrace.implementation.prototype.stringifyArguments([1, 2, 3]), '1,2,3');
+    equals(printStackTrace.implementation.prototype.stringifyArguments([]), '');
+});
+
+test("guessFunctionNameFromLines", function() {
+    expect(3);
+    equals(printStackTrace.implementation.prototype.guessFunctionNameFromLines(2, ['var a = function() {', 'var b = 2;', '};']), 'a');
+    equals(printStackTrace.implementation.prototype.guessFunctionNameFromLines(2, ['function a() {', 'var b = 2;', '};']), 'a');
+    equals(printStackTrace.implementation.prototype.guessFunctionNameFromLines(2, ['var a = 1;', 'var b = 2;', 'var c = 3;']), '(?)');
+});
+
+test("getSource cache miss", function() {
+    expect(3);
+    var p = new printStackTrace.implementation();
+    var file = 'file:///test';
+    p.ajax = function(fileArg, callback) {
+        equals(fileArg, file, 'cache miss');
+        return 'line0\nline1\n';
+    };
+    var lines = p.getSource(file);
+    equals(lines[0], 'line0');
+    equals(lines[1], 'line1');
+});
+
+test("getSource cache hit", function() {
+    expect(2);
+    var p = new printStackTrace.implementation();
+    var file = 'file:///test';
+    p.ajax = function(fileArg, callback) {
+        ok(false, 'not called');
+    };
+    p.sourceCache[file] = ['line0', 'line1'];
+    var lines = p.getSource(file);
+    equals(lines[0], 'line0');
+    equals(lines[1], 'line1');
+});
+
+test("sync ajax", function() {
+    expect(1);
+    var p = new printStackTrace.implementation();
+    var data = p.ajax(document.url);
+    ok(data.indexOf('printstacktrace') >= 0, 'synchronous get');
+});
+
+test("guessFunctionName", function() {
+    expect(1);
+    var p = new printStackTrace.implementation();
+    var file = 'file:///test';
+    p.sourceCache[file] = ['var a = function() {', 'var b = 2;', '};'];
+    equals(p.guessFunctionName(file, 2), 'a');
+});
+
+test("guessFunctionName exception", function() {
+    expect(1);
+    var p = new printStackTrace.implementation();
+    p.getSource = function() {
+        throw 'permission denied';
+    };
+    var file = 'file:///test';
+    equals(p.guessFunctionName(file, 2), 'getSource failed with url: file:///test, exception: permission denied');
+});
+
+test("guessFunctions firefox", function() {
+    var results = [];
+    var mode = printStackTrace.implementation.prototype.mode();
+    var p = new printStackTrace.implementation();
+    p.mode = function() {
+        return 'firefox';
+    };
+    var file = 'file:///test';
+    p.sourceCache[file] = ['var f2 = function() {', 'var b = 2;', '};'];
+    results.push(['{anonymous}()@'+file+':2']);
+        
+    if (mode == 'firefox') {
+        var f2 = function() {
+            try {
+                (0)();
+            } catch(e) {
+                results.push(p.run());
+            }
+        };
+        f2();
+    }
+    
+    expect(results.length * 1);
+    for (var i = 0; i < results.length; ++i) {
+        equals(p.guessFunctions(results[i])[0].indexOf('f2'), 0, 'f2');
+    }
+});
+
+test("guessFunctions opera", function() {
+    var results = [];
+    var mode = printStackTrace.implementation.prototype.mode();
+    var p = new printStackTrace.implementation();
+    p.mode = function() {
+        return 'opera';
+    };
+    var file = 'file:///test';
+    p.sourceCache[file] = ['var f2 = function() {', 'var b = 2;', '};'];
+    results.push(['{anonymous}()@'+file+':2 -- code']);
+    
+    if (mode == 'opera') {
+        var f2 = function() {
+            try {
+                (0)();
+            } catch(e) {
+                results.push(p.run());
+            }
+        };
+        f2();
+    }
+    
+    expect(results.length * 1);
+    for (var i = 0; i < results.length; ++i) {
+        equals(p.guessFunctions(results[i])[0].indexOf('f2'), 0, 'f2');
+    }
+});
+
+test("guessFunctions other", function() {
+    var results = [];
+    var mode = printStackTrace.implementation.prototype.mode();
+    var p = new printStackTrace.implementation();
+    p.mode = function() {
+        return 'other';
+    };
+    var file = 'file:///test';
+    p.sourceCache[file] = ['var f2 = function() {', 'var b = 2;', '};'];
+    results.push(['{anonymous}()']);
+       
+    if (mode == 'other') {
+        var f2 = function() {
+            try {
+                (0)();
+            } catch(e) {
+                results.push(p.run());
+            }
+        };
+        f2();
+    }
+    
+    expect(results.length * 1);
+    for (var i = 0; i < results.length; ++i) {
+        equals(p.guessFunctions(results[i])[0].indexOf('{anonymous}'), 0, 'no file and line number on other');
+    }
+});
